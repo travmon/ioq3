d74ebd655aac983a733a6a0dd083430593b7ee78
travmon <1417786+travmon@users.noreply.github.com>
2019-02-23 08:23:33 -0400

    Try later

--- code/server/sv_ccmds.c
+++ code/server/sv_ccmds.c
@@ -1809,50 +1809,6 @@ static void SV_StartServerDemo_f(void) {
 
 /*
 ==================
-SV_Teleport
-==================
-*/
-static void SV_Teleport_f(void) {
-    client_t *clFrom, *clTo;
-    playerState_t *psFrom, *psTo;
-
-    if (!com_sv_running->integer) {
-        Com_Printf("Server is not running\n");
-        return;
-    }
-
-    if (Cmd_Argc() < 3) {
-        Com_Printf("Usage: teleport <player> <toPlayer>\n");
-        return;
-    }
-
-    clFrom = SV_BetterGetPlayerByHandle(Cmd_Argv(1));
-    if (!clFrom) {
-        Com_Printf("Invalid source player.\n");
-        return;
-    }
-
-    clTo = SV_BetterGetPlayerByHandle(Cmd_Argv(2));
-    if (!clTo) {
-        Com_Printf("Invalid target player.\n");
-        return;
-    }
-
-    psFrom = SV_GameClientNum(clFrom - svs.clients);
-    psTo = SV_GameClientNum(clTo - svs.clients);
-
-    if (psTo->persistant[PERS_TEAM] == TEAM_SPECTATOR) {
-        Com_Printf("Can't teleport to a spectator.\n");
-    }
-
-    VectorCopy(psTo->origin, psFrom->origin);
-    VectorClear(psFrom->velocity);
-
-    Com_Printf("Teleported %s to %s\n", Cmd_Argv(1), Cmd_Argv(2));
-}
-
-/*
-==================
 SV_StopServerDemo_f
 
 Stop a server-side demo for given player/slot. Note that
@@ -2033,8 +1989,6 @@ void SV_AddOperatorCommands( void ) {
 
 	Cmd_AddCommand ("invisible", SV_Invisible_f);
 	Cmd_AddCommand ("invulnerable", SV_Invulnerable_f);
-	Cmd_AddCommand ("teleport", SV_Teleport_f);
-
 
 #ifndef PRE_RELEASE_DEMO
 	Cmd_AddCommand ("devmap", SV_Map_f);
